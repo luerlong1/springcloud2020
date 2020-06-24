77c85b22237f888026280046388f14304e34f146
luerl <luerlong1997@163.com>
2020-06-24 15:51:42 +0800

    code

---
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,36 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 17:22
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable
+{
+    private Long id;
+    private String serial;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+}
